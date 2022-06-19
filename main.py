682d08e8420018925f3c044535fda955d44fc502
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2022-06-19 22:48:52 +0100

    update application to app

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import datetime as dt
 from flask import Flask, render_template
 
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 year = dt.datetime.now().year
 
-@application.route("/")
+@app.route("/")
 def home():
     return render_template("index.html", current_year=year)
 
-@application.route("/about")
+@app.route("/about")
 def about():
     return render_template("about.html", current_year=year)
 
-@application.route("/contact")
+@app.route("/contact")
 def contact():
     return render_template("contact.html", current_year=year)
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    app.run(debug=True)
