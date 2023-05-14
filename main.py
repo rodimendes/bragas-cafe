e59df3a79da78af46a4819e30ab3b8747fbf3099
Rodrigo Pinto <rodimendes@gmail.com>
2023-05-14 10:36:54 +0100

    Updating

--- main.py
+++ main.py
@@ -72,5 +72,5 @@ def cafes():
 ## FUNCTION TO DELETE A CAFE ##
 ###############################
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000, host="0.0.0.0")
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5000, host="0.0.0.0")
